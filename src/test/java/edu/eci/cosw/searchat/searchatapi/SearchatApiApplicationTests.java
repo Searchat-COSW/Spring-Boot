6b75826d76612d34f40a6aff98ba073f69029720
Hernan Felipe Losada <hernan.losada96@gmail.com>
2018-02-28 10:04:30 -0500

    add Test

--- src/test/java/edu/eci/cosw/searchat/searchatapi/SearchatApiApplicationTests.java
+++ src/test/java/edu/eci/cosw/searchat/searchatapi/SearchatApiApplicationTests.java
@@ -1,7 +1,18 @@
 package edu.eci.cosw.searchat.searchatapi;
 
+import edu.eci.cosw.searchat.searchatapi.model.Activity;
+import edu.eci.cosw.searchat.searchatapi.model.ProfileInformation;
+import edu.eci.cosw.searchat.searchatapi.model.User;
+import edu.eci.cosw.searchat.searchatapi.service.ActivityService;
+import edu.eci.cosw.searchat.searchatapi.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import org.junit.Assert;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +20,144 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SearchatApiApplicationTests {
 
+        @Autowired
+        UserService userservice;
+    
+        @Autowired
+        ActivityService activitiservie;
+        
 	@Test
-	public void contextLoads() {
+	public void CreateAndCheckNewUser() {
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            try{
+                Assert.assertEquals(userservice.createUser(tmp),true);
+                User newUserCheck = userservice.getUser("Pipe");
+                Assert.assertEquals(tmp.getEmail(), newUserCheck.getEmail());
+                Assert.assertEquals(tmp.getFirstname(), newUserCheck.getFirstname());
+                Assert.assertEquals(tmp.getLastname(), newUserCheck.getLastname());
+                Assert.assertEquals(tmp.getPassword(), newUserCheck.getPassword());
+                Assert.assertEquals(tmp.getUsername(), newUserCheck.getUsername());
+            }catch(ServletException ex){
+                fail("Fail in CreateUser");
+            }
+	}
+        
+        @Test
+	public void CantBeTwoUserWithTheSameUserName() {
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            try{
+                Assert.assertEquals(userservice.createUser(tmp),true);
+                userservice.createUser(tmp);
+                fail("Fail in CantBeTwoUserWithTheSameUserName");
+            }catch(ServletException ex){
+                
+            }
 	}
 
+        @Test
+        public void updateProfileInformationUser(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            ProfileInformation tmp_profile = new ProfileInformation("Colombia", "Spanish English German", "This its a prove", "http://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/01/26/15169827142899.jpg");
+            try{
+                Assert.assertEquals(userservice.updateProfileInformation("Pipe", tmp_profile), true);
+                User newUserCheck = userservice.getUser("Pipe");
+                Assert.assertEquals(tmp.getProfileInformation().getAboutYou(),newUserCheck.getProfileInformation().getAboutYou());
+                Assert.assertEquals(tmp.getProfileInformation().getImage(),newUserCheck.getProfileInformation().getImage());
+                Assert.assertEquals(tmp.getProfileInformation().getLanguages(),newUserCheck.getProfileInformation().getLanguages());
+                Assert.assertEquals(tmp.getProfileInformation().getNationality(),newUserCheck.getProfileInformation().getNationality());
+            }catch(ServletException e){
+                fail("Fail in UpdateProfileInformationUser");
+            }
+        }
+        
+        @Test
+        public void getAllUser(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            User tmp2 = new User("Felipe", "fel@mail.com", "password", "Feliipe", "Calderon", null);
+            User userByDefect = new User( "xyz","test@mail.com", "password", "Andres", "Perez", "https://ams.educause.edu/eweb/upload/60283746.jpg" );
+            List<User> tmp_users = new ArrayList<>();
+            tmp_users.add(userByDefect);
+            tmp_users.add(tmp);
+            tmp_users.add(tmp2);
+            try{
+                Assert.assertEquals(userservice.createUser(tmp),true);
+                Assert.assertEquals(userservice.createUser(tmp2),true);
+                List<User> newUsersCheck = userservice.getUsers();
+                for(int i = 0; i  < newUsersCheck.size(); i++){
+                    Assert.assertEquals(tmp_users.get(i).getEmail(), newUsersCheck.get(i).getEmail());
+                    Assert.assertEquals(tmp_users.get(i).getFirstname(), newUsersCheck.get(i).getFirstname());
+                    Assert.assertEquals(tmp_users.get(i).getLastname(), newUsersCheck.get(i).getLastname());
+                    Assert.assertEquals(tmp_users.get(i).getPassword(), newUsersCheck.get(i).getPassword());
+                    Assert.assertEquals(tmp_users.get(i).getUsername(), newUsersCheck.get(i).getUsername());
+                }
+            }catch(ServletException ex){
+                fail("Fail in getAllUser");
+            }
+        }
+        
+        
+        @Test
+        public void createNewActivity(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            Activity activity_tmp = new Activity("prueba", "its a prove", tmp, "English", "Bogota", null, new ArrayList<User>(), "10");
+            try{
+                userservice.createUser(tmp);
+                Assert.assertEquals(activitiservie.createActivity(activity_tmp),true);
+                Activity checkActivity = activitiservie.getActivity("prueba");
+                Assert.assertEquals(activity_tmp.getName(), checkActivity.getName());
+                Assert.assertEquals(activity_tmp.getDate(), checkActivity.getDate());
+                Assert.assertEquals(activity_tmp.getDescription(), checkActivity.getDescription());
+                Assert.assertEquals(activity_tmp.getLanguages(), checkActivity.getLanguages());
+                Assert.assertEquals(activity_tmp.getPrice(), checkActivity.getPrice());
+                Assert.assertEquals(activity_tmp.getAdministrator().toString(), checkActivity.getAdministrator().toString());
+            }catch(ServletException ex){
+                fail("Fail in CreateNewActivity");
+            }
+        }
+        
+        @Test
+        public void cantBeTwoActivityWithTheSameName(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            Activity activity_tmp = new Activity("prueba", "its a prove", tmp, "English", "Bogota", null, new ArrayList<User>(), "10");
+            try{
+                userservice.createUser(tmp);
+                Assert.assertEquals(activitiservie.createActivity(activity_tmp),true);
+                activitiservie.createActivity(activity_tmp);
+                fail("Fail in cantBeTwoActitvityWitTheSameName");
+            }catch(ServletException ex){
+            }
+        }
+        
+        @Test
+        public void addNewParticipant(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            User tmp2 = new User("Felipe", "fel@mail.com", "password", "Feliipe", "Calderon", null);
+            Activity activity_tmp = new Activity("prueba", "its a prove", tmp, "English", "Bogota", null, new ArrayList<User>(), "10");
+            try{
+                userservice.createUser(tmp);
+                userservice.createUser(tmp2);
+                Assert.assertEquals(activitiservie.createActivity(activity_tmp),true);
+                Assert.assertEquals(activitiservie.joinActivity("prueba", tmp2),true);
+                Activity checkActivity = activitiservie.getActivity("prueba");
+                Assert.assertEquals(checkActivity.getParticipants().get(0).toString(), tmp2.toString());
+            }catch(ServletException ex){
+                fail("fail in addNewParticipant");
+            }
+        }
+        
+        @Test
+        public void cantBeAddAdministratorToActivity(){
+            User tmp = new User("Pipe", "pip@mail.com", "password", "Felipe", "Losada", null);
+            Activity activity_tmp = new Activity("prueba", "its a prove", tmp, "English", "Bogota", null, new ArrayList<User>(), "10");
+            try{
+                userservice.createUser(tmp);
+                Assert.assertEquals(activitiservie.createActivity(activity_tmp),true);
+                Assert.assertEquals(activitiservie.joinActivity("prueba", tmp), true);
+                activitiservie.joinActivity("prueba", tmp);
+                fail("Fail in cantBeAddAdministratorToActivity");
+            }catch(ServletException ex){
+            }
+        }
+        
+     
 }
